646794f71181061a752ac92616bf110457665de0
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-31 09:52:44 -0800

    Task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/(\[from:(.+)\]|\[to:(.+)\]|\[flags:(.+)\])/).join
+
 $from = ARGV[0].scan(/\[from:(.+?)\]/).join()
 $to = ARGV[0].scan(/\[to:(.+?)\]/).join()
 $flags = ARGV[0].scan(/\[flags:(.+?)\]/).join()
+
 puts "#$from,#$to,#$flags"
