fbca759a6565365a1cc76dad39f932b3d3ed3c29
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-04 08:43:27 +0000

    Task 100

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,7 +13,7 @@ package { 'nginx':
 file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  line   => "\tadd_header X-Served-By ${hostname};",
+  line   => "\t\tadd_header X-Served-By ${hostname};",
   after  => 'location / {',
 }
 
