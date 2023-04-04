8c84c7e7ac4f4d691165983d948feb6009dfa747
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-03 19:44:18 -0700

    Task 100

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# Add a custom HTTP header with Puppet
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file_line { 'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'add_header X-Served-By ${hostname}',
+  after  => 'location / {',
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  require => Pakage['nginx'],
+}
