72734f21aaf828311121a7720e26bdfc0a94e37e
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-24 16:25:19 +0100

    Task 00

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Using REST API, for a given employee ID, returns information about
+his/her TODO list progress.
+"""
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+
+    #  get args
+    userId = int(argv[1])
+
+    #  Fetch todos and user
+    todos = get('https://jsonplaceholder.typicode.com/todos').json()
+    user = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+               userId)).json()
+
+    #  Extract user specific todos
+    user_todos = []
+    for todo in todos:
+        if todo['userId'] == userId:
+            user_todos.append(todo)
+
+    #  get todos analytics
+    todos_total = len(user_todos)
+    todos_completed = sum(todo['completed'] for todo in user_todos)
+
+    print(todos_total)
+    print(todos_completed)
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+          user.name, todos_completed, todos_total))
+    for todo in [todo for todo in users_todos if todo['completed']]:
+        print('\t {}'.format(todo['title']))
