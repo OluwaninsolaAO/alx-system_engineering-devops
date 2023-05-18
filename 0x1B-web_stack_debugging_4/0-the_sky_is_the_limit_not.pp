e528338ee757881ef2976e84f67419d1f41393fc
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-05-18 12:54:09 +0100

    Task 00

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,8 +2,10 @@
 
 exec { 'increase-ulimit':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin/', '/bin/'],
 }
 
 exec { 'restart-nginx':
-  command => 'service nginx restart',
+  command => 'nginx restart',
+  path    => ['/etc/init.d/'],
 }
