a83a19a1df0f3e4b24fed146baae3027f6c32a96
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-05-18 12:51:19 +0100

    Task 00

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # 0. Sky is the limit, let's bring that limit higher
 
 exec { 'increase-ulimit':
-   command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
 }
 
 exec { 'restart-nginx':
-   command => 'service nginx restart',
+  command => 'service nginx restart',
 }
